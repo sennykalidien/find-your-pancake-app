fa98b2b3629e7907a51316f67c405414f5578b49
Senny Kalidien <sennykalidien@gmail.com>
2019-05-19 00:28:58 +0200

    Setup first container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './static/images/logo.svg';
+import './App.scss';
+import HomeContainer from './containers/HomeContainer/HomeContainer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="Appheader">
+                <img src={logo} className="AppLogo" alt="logo"/>
+            </header>
+            <HomeContainer />
+        </div>
+    );
 }
 
 export default App;
